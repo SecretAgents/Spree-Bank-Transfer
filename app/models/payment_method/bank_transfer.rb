901ce1da74aa605efe83372c8283277edebe6781
Igor Narazin <in@isfb.ru>
2014-05-15 15:01:17 +0400

    fix and new status

--- app/models/payment_method/bank_transfer.rb
+++ app/models/payment_method/bank_transfer.rb
@@ -10,7 +10,7 @@ class PaymentMethod::BankTransfer < Spree::PaymentMethod
 
   # Indicates whether its possible to capture the payment
   def can_capture?(payment)
-    ['checkout', 'pending'].include?(payment.state)
+    ['invoice'].include?(payment.state)
   end
   
   # Indicates whether its possible to void the payment.
@@ -18,16 +18,12 @@ class PaymentMethod::BankTransfer < Spree::PaymentMethod
     payment.state != 'void'
   end
   
-  def capture(payment)
-    payment.update_attribute(:state, 'pending') if payment.state == 'checkout'
-    payment.complete
-    true
+  def capture(*args)
+    ActiveMerchant::Billing::Response.new(true, "", {}, {})
   end
   
-  def void(payment)
-    payment.update_attribute(:state, 'pending') if payment.state == 'checkout'
-    payment.void
-    true
+  def void(*args)
+    ActiveMerchant::Billing::Response.new(true, "", {}, {})
   end
   
   def source_required?
@@ -35,3 +31,4 @@ class PaymentMethod::BankTransfer < Spree::PaymentMethod
   end
 
 end
+
